f45b355ff3c80371a5fa19f966b54bfd63571954
Mateusz Hyla <hylam@interia.pl>
2017-04-23 15:31:22 +0200

    Add user class nad data

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+
+//Stwórz poniżej odpowiednie zmienne z danymi do bazy
+$user = 'root';
+$password = 'coderslab';
+$dbname = 'products_ex';
+$hostname = 'localhost';
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
---
+++ public/connection.php
@@ -0,0 +1,16 @@
+<?php
+include_once '../config/db.php';
+//Poniżej napisz kod łączący się z bazą danych
+$connection = new PDO(sprintf("mysql:host=%s;dbname=%s", $hostname, $dbname), $user, $password);
+
+if ($connection->errorCode() != null) {
+    var_dump($connection->errorInfo());
+    die();
+}
+echo 'Połączenie udane';
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
